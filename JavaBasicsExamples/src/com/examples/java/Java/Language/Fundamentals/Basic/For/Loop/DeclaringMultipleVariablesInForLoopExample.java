861a9d17fcf38affabc282d50f3e7787afddc6d1
PanosPapazoglou <PANOS_PAPAZOGLOY@HOTMAIL.COM>
2012-11-30 15:47:24 +0200

    .

--- JavaBasicsExamples/src/com/examples/java/Java/Language/Fundamentals/Basic/For/Loop/DeclaringMultipleVariablesInForLoopExample.java
+++ JavaBasicsExamples/src/com/examples/java/Java/Language/Fundamentals/Basic/For/Loop/DeclaringMultipleVariablesInForLoopExample.java
@@ -8,21 +8,22 @@ declaration block.
 
 public class DeclaringMultipleVariablesInForLoopExample {
 
-public static void main(String[] args) {
- 
-  /*
-   * Multiple variables can be declared in declaration block of for loop.
-   */
-   
-   for(int i=0, j=1, k=2 ; i<5 ; i++)
-     System.out.println("I : " + i + ",j : "+ j + ", k : " + k);
-   
-   /*
-    * Please note that the variables which are declared, should be of same type
-    * as in this example int.
-    */  
-   
-    //THIS WILL NOT COMPILE
-    //for(int i=0, float j; i < 5; i++);
-}
+	public static void main(String[] args) {
+	 
+	  /*
+	   * Multiple variables can be declared in declaration block of for loop.
+	   */
+	   
+	   for(int i=0, j=1, k=2 ; i<5 ; i++)
+	     System.out.println("I : " + i + ",j : "+ j + ", k : " + k);
+	   
+	   /*
+	    * Please note that the variables which are declared, should be of same type
+	    * as in this example int.
+	    */  
+	   
+	    //THIS WILL NOT COMPILE
+	    //for(int i=0, float j; i < 5; i++);
+	}
+	
 }
