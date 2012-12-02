8f7cfd87795d356da74aba21c2a978857376ed84
panos <panos@panos-PC.lan>
2012-12-02 20:20:18 +0200

    Commonly Used Java Classes » Java Date Class

---
+++ JavaBasicsExamples/src/com/javaexamples/CommonlyUsedJavaClasses/JavaDateClass/ConvertDateIntoMilliSecondsExample.java
@@ -0,0 +1,33 @@
+package com.javaexamples.CommonlyUsedJavaClasses.JavaDateClass;
+
+/*
+Convert Date into milliseconds example
+This example shows how to get the milliseconds since January 1, 1970, 00:00:00 GMT
+represented by this Date object, using getTime() method of java Date class
+*/
+
+import java.util.*;
+
+public class ConvertDateIntoMilliSecondsExample{
+
+public static void main(String args[]){
+
+//create a Date object  
+Date date = new Date();
+System.out.println("Date is : " + date);
+
+long  timestamp = date.getTime()/1000;
+
+//use getTime() method to retrieve milliseconds
+System.out.println("Milliseconds since January 1, 1970, 00:00:00 GMT : "
+                    + date.getTime()+"("+timestamp+")");
+ 
+}
+
+}
+
+/*
+TYPICAL Output would be
+Date is : Sun Sep 09 19:56:42 EDT 2007
+Milliseconds since January 1, 1970, 00:00:00 GMT : 1189382202606
+*/
