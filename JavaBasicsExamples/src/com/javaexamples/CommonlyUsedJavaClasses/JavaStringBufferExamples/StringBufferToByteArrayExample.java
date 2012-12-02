5f6a60a5e12ba71aaf7a540fecf3b3cb31793ead
panos <panos@panos-PC.lan>
2012-12-03 00:57:04 +0200

    Commonly Used Java Classes » Java StringBuffer Examples

---
+++ JavaBasicsExamples/src/com/javaexamples/CommonlyUsedJavaClasses/JavaStringBufferExamples/StringBufferToByteArrayExample.java
@@ -0,0 +1,30 @@
+package com.javaexamples.CommonlyUsedJavaClasses.JavaStringBufferExamples;
+
+/*
+StringBuffer To byte Array Java Example
+This example shows how to convert StringBuffer to byte Array in Java.
+*/
+
+
+public class StringBufferToByteArrayExample {
+
+	public static void main(String[] args) {
+	       
+	        //create StringBuffer object
+	        StringBuffer sbf = new StringBuffer("Java StringBuffer To byte array example");
+	       
+	        /*
+	         * To Convert StringBuffer to byte array, first convert StringBuffer
+	         * to String object using toString method of String class.
+	         *
+	         * Once it is converted to String object, use
+	         * byte[] getBytes() method of String class to convert it into
+	         * byte array.
+	         */
+	       
+	        byte bytes[] = sbf.toString().getBytes();
+	       
+	        System.out.println("StringBuffer converted to byte array");
+	}
+	
+}
