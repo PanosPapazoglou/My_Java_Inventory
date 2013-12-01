ef90020969bf7edae6072f857e27256e80f3d150
panos <panos@panos-PC>
2013-12-01 16:00:12 +0200

    Database (mysql) Connection and Retrieve Data from db (Incluced persons.sql)

---
+++ JavaBasicsExamples/src/database/DBConnect.java
@@ -0,0 +1,66 @@
+package database;
+
+import java.sql.*;
+
+
+public class DBConnect {
+	
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	//Constructor
+	public DBConnect(){
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			// Create a Connection
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_dbcon","root","");
+			
+			// Create a Statement
+			st = con.createStatement();
+			
+			
+					
+		}catch(Exception ex){
+			System.out.println("Error: "+ex);
+		}
+	}
+	
+	
+	//This method retrieves the records from the database and bring them up
+	public void getData(){
+	
+		try{
+			
+			// We write our query
+			String query = "Select * from persons";
+			
+			// We get the resultset from the db
+			rs = st.executeQuery(query);
+			
+			//Print out the mesage
+			System.out.println("Records from Database");
+			
+			// As long as rs has records...gather them and bring them out..
+			while (rs.next()){
+				// Column Name
+				String name = rs.getString("name");
+				
+				// Column Age
+				String age = rs.getString("age");
+				
+				//Print each row data
+				System.out.println("Name: "+name+"    "+"Age:"+age);
+				
+			}
+			
+			
+		}catch(Exception ex){
+			System.out.println("Error:"+ex);
+		}
+		
+	}
+	
+	
+}
---
+++ JavaBasicsExamples/stuff/persons.sql
@@ -0,0 +1,31 @@
+/*
+MySQL Backup
+Source Host:           localhost
+Source Server Version: 5.6.12-log
+Source Database:       java_dbcon
+Date:                  2013/12/01 15:58:17
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+use java_dbcon;
+#----------------------------
+# Table structure for persons
+#----------------------------
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `age` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+#----------------------------
+# Records for table persons
+#----------------------------
+
+
+insert  into persons values 
+(1, 'panos', '35'), 
+(2, 'nikos', '34'), 
+(3, 'petros', '33'), 
+(4, 'spyros', '35'), 
+(5, 'kostas', '32');
+
