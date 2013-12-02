aa8e32f0111f4b503a7ed6a452417cf69d5e6f5a
panos <panos@panos-PC>
2013-12-02 20:54:13 +0200

    allagi lektikou sto minima

--- JavaBasicsExamples/src/org/creativitytuts/Input.java
+++ JavaBasicsExamples/src/org/creativitytuts/Input.java
@@ -7,6 +7,6 @@ public class Input {
 		public static void main(String[] args){
 			String a;
 			a = JOptionPane.showInputDialog("Please enter your name!");
-			JOptionPane.showMessageDialog(null, "Your name is "+a);
+			JOptionPane.showMessageDialog(null, "Hello "+a);
 		}
 }
