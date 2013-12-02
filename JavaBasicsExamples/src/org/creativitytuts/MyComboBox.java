40b70cb1ef6db9f302bc0dd2fc841209a371c43b
panos <panos@panos-PC>
2013-12-02 22:58:31 +0200

    Create combobox and having an action listener on o button in order to set the labels text of what the combobox selected item is , as soon as we click the button..

---
+++ JavaBasicsExamples/src/org/creativitytuts/MyComboBox.java
@@ -0,0 +1,63 @@
+package org.creativitytuts;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class MyComboBox {
+	
+	String[] items = {"Panos","Nikos","Petros","Spyros","Kostas","Mixalis","Takis"};
+	JComboBox c = new JComboBox(items);
+	JButton b 	= new JButton("Print"); 
+	JLabel l	 	= new JLabel("Display Combo items");	
+
+	
+	public MyComboBox(){
+		frame();
+	}
+
+	
+	public void frame(){
+		JFrame f = new JFrame();
+		f.setSize(400, 400);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+		JPanel p = new JPanel();
+
+
+		
+		p.add(c);
+		p.add(b);
+		p.add(l);
+		
+		f.add(p);
+		
+		
+		
+	  // Edo kanoume register to component ston listener pou ftaixnoume sto ftero..
+		b.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent ev){
+				String s = c.getSelectedItem().toString();
+				l.setText(s);
+				
+				
+			}
+			
+			
+		});
+		
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args){
+		new MyComboBox();
+		
+	}
+	
+}
