b2bee99d0fd225501efad94c7f5a3358b9fe7bda
panos <panos@panos-PC>
2013-12-03 21:31:00 +0200

    Pos na ftiaxnoume toolbar (To MyToolBar.java einai me extend to Jframe) (Eno to MyToolBar2.java einai apla xoris na to kanoume extend..)

---
+++ JavaBasicsExamples/src/org/creativitytuts/MyJtoolBar.java
@@ -0,0 +1,41 @@
+package org.creativitytuts;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class MyJtoolBar extends JFrame {
+
+	JToolBar tb;
+	JButton exit;
+	
+	
+	public MyJtoolBar() {
+		
+		setSize(400,400);
+		setTitle("ToolBar test");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	
+		tb = new JToolBar();
+		exit = new JButton("Exit");
+		
+		tb.add(exit);
+		
+		// An den theloume na einai dragable
+		// tb.setFloatable(false);
+		
+		
+		
+		add(tb,BorderLayout.NORTH);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		MyJtoolBar t = new MyJtoolBar();
+		t.setVisible(true);
+
+	}
+
+}
---
+++ JavaBasicsExamples/src/org/creativitytuts/MyJtoolBar2.java
@@ -0,0 +1,54 @@
+package org.creativitytuts;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+
+public class MyJtoolBar2 {
+
+	JFrame f;
+	JToolBar tb;
+	JButton exit;
+	
+	public  MyJtoolBar2(){
+		frame();
+	}
+	
+	
+	public void frame(){
+
+		f = new JFrame();
+		f.setSize(400,400);
+		f.setTitle("ToolBar test");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+		tb = new JToolBar();
+		
+		exit = new JButton("Exit");
+		
+		tb.add(exit);
+		
+		// An den theloume na einai dragable
+		// tb.setFloatable(false);
+		
+		
+		
+		f.add(tb,BorderLayout.NORTH);
+	
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args){
+		new  MyJtoolBar2();
+	}
+	
+	
+	
+	
+	
+}
