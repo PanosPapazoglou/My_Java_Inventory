72f043b860ee93ef3a0b6d046fa5d775f56f0b6f
panos <panos@panos-PC>
2013-12-06 14:49:46 +0200

    Before Applying our "Seperate Vary assets from nonChangable ones" Abstruct Class : Duck

---
+++ DesignPatterns/src/SeperateVaryFromNonChangable/before/Duck.java
@@ -0,0 +1,25 @@
+package SeperateVaryFromNonChangable.before;
+
+public abstract class Duck {
+
+	public void quack(){
+		
+	}
+	
+	
+	public void swim(){
+		
+	}
+	
+	
+	public void display(){
+		
+	}
+	
+	
+	// Other duck-like methods
+	
+	
+	
+	
+}
