4380e80768ae4b200aaac08fe8b15e169a3bcd70
panos <panos@panos-PC>
2013-12-07 01:16:08 +0200

    Sxolia sxetika me to pos kataligoume kai ilopoioume to sigkekrimeno Design Pattern feygontas apo to Inheritance kai kataligontas sto Encapsulation kai sto Polymorphism

--- DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade2/try4/Duck.java
+++ DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade2/try4/Duck.java
@@ -55,6 +55,9 @@
  *                 -> Take the parts that vary and encapsulate them, 
  *                 -> so that later you can alter or extend the parts tha vary .
  *                 -> without affecting those that don't!!
+ *                 
+ *                 * Prokeite gia tin basi olon ton Design Principles - 
+ *                   Diladi na mporoume na tropopoioume merika komatia tou kodika/sistimatos XORIS na epireazontai ta alla!!
  *             
  * Solution 4 : Basismenoi loipon sto 1o Sxediastiko kanona tha prepei na xexorisoume ayta pou allazoun apo ayta pou paramenoun idia
  *              Epomenos idia menoun edo to pos kolimpane oles oi papies
@@ -64,11 +67,113 @@
  *              
  *              Me ta parapano katalabainoume oti to petagma fly() kai o ixos quack() allazoun kai tha allazoun sinexos kai sto mellon
  *              epomenos tha prepei na ta apokentrosoume - diladi na ta bgaloume ektos tis klasis Duck os behavious (os methodoi)
+ *              
+ *              Pio sigkekrimena : 
+ *              a) Tha bgaloume to behavior (tis methodous) fly() kai quack() ektos tis klasis Duck
+ *              b) Tha ftiaxoume 2 set klasseon ena gia to petagma kai ena gia ton ixo pou bgazoun
+ *                 Kathe mia apo aytes tis klaseis tha emperiexoun tin ekastote ilopoiisi tis simperiforas pou tin aforoun 
+ *                 (eache set of classes will hole all the implementation of their respctive bejavior)
+ *                 i allios..tha dimiourgisoume ena neo set klaseon pou tha antiprosopeyei tin ekastote simperifora
+ *                 Etsi gia to 1o SET pou afora sto QUACK : 
+ *                    i) tha exoume mia klasi pou tha ilopoiei to "quacking"
+ *                   ii) alli mia klasi pou tha ilopoiei (implements) to "squeaking"
+ *                  iii) kai alli mia klasi pou tha ilopoiei (implements) to "silence"
+ *                  
+ *              
+ *              Theloume loipon ta pragmata na einai flexible , 
+ *              kai theloume na kanoume assign behaviors sta instances enos Duck
  *               
+ *              Diladi tha thelame na mpoorume na kanoume instantiate ena MallardDuck Instance 
+ *              kai na to kanoume initialize oste na tou prosdidoume mia sigkekrimeni simperifora petagmatos (flying behavior)
+ *              
+ *              Kai mia kai tha eimaste se ayto to sxediastiko simeio..giati na min mporoume kai na allazoume kai tin simperifora ayti kai sto runtime
+ *              Diladi..gia tin na min efarmosoume Getters kai Setters methodous stin Duck Class (Encapsulation i alli themelliodis arxi tou OO Programming)
+ *                 oste na mporoume na allazoume tin simperifora petagmatos (fly behavior) p.x. tou eidous MallardDuck kai kata to runtime!!
+ *                 
+ *                 
+ *                 
+ *                 
+ *              
+ *              Exontas os stoxo ta parapano..as doume ton 2o Design Principle.. 
+ * Design 
+ * Principle (2) : Program to an Interface , NOT an Implementation
+ *                 (ayto gia na einai oso to dinaton pio abstract oste p.x. mesa sto duck class i sta subclases tou na min xreiazontai 
+ *                  idiaiteres dieykriniseis kai ara programamtistikes tropoopoiseis kai diafores..)             
  *              
  *              
  *              
  *              
+ *              c) Tha xrisimopoiisoume loipon 2 interfaces* (ta 2 sets..pou legame parapano)  
+ *                 gia paradeigma "FlyBehavior" kai "QuackBehavior" 
+ *                 
+ *                 (*me tin genikoteri ennoia ..dhladh Interface i Abstract class..stin java)
+ *                 
+ *              d) KAI kathe ilopoiisi mias ek ton simperiforon (behavior) tha ilopoiiei ena apo ayta ta 2 interfaces
+ *                 Etsi apo do kai pera oi simperifores tis papias Duck behaviors (fly,quack) tha zoun se mia xexoristi klasi
+ *                 i opoia tha prepei na ilopoiei (implement) ena sigkekrimeno behavior interface!!
+ *                 (diladi tha emploutisoume ton kodika pou grapsame sto bima b se sxesi me to c)
+ *                 
+ *   
+ *   APOTELESMA : Etsi oi DUCK CLASSES den tha xreiazetai na gnorizoun KAMIA LEPTOMERIA sxetika me tin YLOPOISI tis idias tous tis SYMPERIFORAS!!!
+ *                 
+ *   
+ *   EPOMENOS...edo erxomaste se antithesi me ayto pou kaname mexri tora 
+ *              diladi na exoume to behavior eite mesa stin superclass Duck
+ *              eite na to exoume mesa stis concrete subclasses (sta eidi ton papion)
+ *              
+ *              Ant' aytou oi simperifores eina xexoristes klaseis oi opoies ilopoioun tis eyriteres ennoies simperforon (fly,quack) ilopoiontas ayta ta enoiologika behavior interfaces
+ *              
+ *              Me to neo mas Design oi Duck subclasses tha xrisimopoouin tin simperifora pou parousiazete apo ena interface (FlyBehavior and QuackBehavior) 
+ *              (etsi kanoume xrisi tis 3is themeliodis arxis tou OO Programming - ayto tou polimorfismou)
+ *              Etsi i pragmatiki ilopoiisi tis simperiforas (i epi tis ousias simperifora tou ekastote eidous eite gia to fly eite gia to quack) 
+ *              den tha eiani egklobismeno mesa sto ekastote Duck subclass (diladi sto ekastote eidos papias) 
+ *              
+ *              H xrisi tou  interface (FlyBehavior and QuackBehavior)  einai enoiologiki!! Tha mporouse stin Java na einai eite ena Java Interface eite mia Abstract class!! 
+ *                                               ### PROGRAM TO AN INTERFACE really means PROGRAM TO A SUPERTYPE ###
+ *                                               
+ *          
+ *                                                         
+ *                                                                     TELIKA ME APLA LOGIA 
+ *                                                    Antikathistoume tin logiki tou "I N H E R I T A N C E" 
+ *                                            me ayti tou "E N C A P S U L A T I  O N"  kai tou "P O L Y M O R F I S M"
+ *              
+ *              
+ *             H ousia einai na ekmetaleytoume ton polumorfismo oste na programamtisoume se ena supertype (Java interface i Java abstract class den exei simasia..)
+ *             etsi oste to runtime object mas na min einai egklobismeno ston kodika alla stin xrisi enos "tilekontrol"
+ *             Kai ti simainei stin Java..tilekontrol;;; METABLITH!!! Dhladh tha antikatastisoume tis palies methodous fly() kai quack() to behavior diladi
+ *             me state ..me instance variable sigkekrimenou omos tipou!!
+ *             
+ *             Kai gia na to poume kai me diaforetika logia....oste na katalixoume kai sto epomeno bima..(to pos denoume diladi ta parapano me tin superklasi mas Duck..)
+ *             Programm to supertype simaieni oti o dilothen tupos (to tilekontrol!!!)  ton metabliton tha prepei na einai supertype - sinithos mia abstract class i eina interface
+ *             oste ta antikeimena opou ginontai assgned se aytes tis metablites na mporoun na einai opoiasdipote ilopoiiseis tou supertype pou ta dilonoume!!
+ *             Auto me tin seira tou simainei oti oi klaseis pou ta orizoun ayta ta instance variables den xreiazete na xeroun apo poies concrete class simperiforas tha ilopoithoun telika
+ *             
+ *             Kai gia na thimithoume tin diafora..
+ *             
+ *                                                             --> Programming to an Implementation <--
+ *             Dog d = new Dog();
+ *             d.bark();
+ *             Ara thetontas tin metabliti se ena typo Dog (pou afora se ena concrete implementation enos Animal) 
+ *             mas anagkazei na grapsoume kodika gia tin sigkekrimeni ilopoiisi aytou tou antikeimenou diladi d.bark i an itan gata d.niaou
+ *             
+ *             
+ *             eno..
+ *             
+ *                                                             --> Programming to an Interface <--
+ *             Animal animal = new Dog();
+ *             animal.makesound();
+ *             Xeroume oti einai skilos...alla tora mporoume na xrisimopoiisoume to animal reference sto na bgalei ixo to zoo/skilos polimorfika!!
+ *             Xrisimopoiontas diladi tin methodo tou interface oxi tou sigkekrimenou hard/coded ekastote..limited/egklobismenou implementation..diadli tou skilou!!
+ *             
+ *                           
+ *             kai malista akomi kalitera tha mporousame..na apofigoume kai tin entoli pou kanei anafora sto concrete object new Dog pou kai ayto mas dimiourgei limitation kai hardcode..
+ *             a = getAnimal();
+ *             a.makeSound();
+ *             
+ *             Diladi tha epafiemetha stin methodo getAnimal na mas epistrepsei to katalilo object oste meso tou abstract methodo tou interface mas ayto to animal na bgalei ixo..
+ *             Emeis diladi den xeroume poio einai to pragmatiko animal subtype..to mono pou mas noiazei einai oti exoume to tilekontrol oste na to kanoume na skouxei..
+ *                          
+ *             
  *  -----------------------------------------------------------------------------------------------------------------------------------
  * New Spec 1: H anabathmisi pou mas zitithike exei na kanei me tin dinatotita tis papias na petaei 
  * Solution 1: Etsi apofasizoume stin abstract klasi na baloume tin methodo fly() stin logiki ton quack() kai swim()
