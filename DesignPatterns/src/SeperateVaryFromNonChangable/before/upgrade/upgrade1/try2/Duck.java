61f0cd02513307d29d47895884bf6e996fb2e31d
panos <panos@panos-PC>
2013-12-07 18:08:49 +0200

    MikroDiorthoseis

--- DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade1/try2/Duck.java
+++ DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade1/try2/Duck.java
@@ -6,17 +6,27 @@ public abstract class Duck {
 		// TODO Auto-generated constructor stub		
 	}
 
-	// ## All Ducks Swim - Thats why we put the code here in the abstract class ##
+	 // -- All Ducks quacks and Swim - Thats why we put the code here in the abstract class
+	 // Generaly code for a duck to quack
+	 public void quack(){
+	   System.out.println("I am quaking!");
+	 }
+	 	 
   // Generaly code for a duck to swim	
 	public void swim(){
 		System.out.println("I am swimming!");
 	}
 
+	 //     ++ New Spec Code (Starts)
+	 // Generaly code for a duck to swim
+	 public void fly(){
+	   System.out.println("I am flying;");
+	 }
+	 //     ++ New Spec Code (Ends)	
+
 	
-	//-- Each Duck subtype is responsible for implementing its own display() behavior for how it looks on the screen
 	
-	
-	
+	 //-- Each Duck subtype is responsible for implementing its own display() behavior for how it looks on the screen	
 
 	// Ftiaxnoume mia abstract methodo gia to abstract class Duck i opoia tha prepei na ilopoiithei apo ta subcalsses tou Duck
   public abstract void display();
--- DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade1/try2/readme.txt
+++ DesignPatterns/src/SeperateVaryFromNonChangable/before/upgrade/upgrade1/try2/readme.txt
@@ -3,7 +3,7 @@
  * -----------------------------------------
  * New Spec 1: H anabathmisi pou mas zitithike exei na kanei me tin dinatotita tis papias na petaei 
  * Solution 1: Etsi apofasizoume stin abstract klasi na baloume tin methodo fly() stin logiki ton quack() kai swim()
- *            etsi oles oi papies na exoun ayti tin dinatotita otan kanoume subclasses
+ *             etsi oles oi papies na exoun ayti tin dinatotita otan kanoume subclasses
  *
  * Problem 1 : Distixos me ayton ton tropo mporoun na petaxoun kai eidi papias pou den mporoun na petaxoun!!!
  * Solution2 : San 2i lisi loipon prokreinete to na  kanei OVERRIDE mesa stis CONCRETE CLASSES (diladi tis klasis ton eidon tis papias)
