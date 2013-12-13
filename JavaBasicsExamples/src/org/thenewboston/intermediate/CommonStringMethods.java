9a87426a1ad607a39fe43ef79984d333c818059c
panos <panos@panos-PC>
2013-12-13 21:45:40 +0200

    Common String Methods startWith() and endWith() methods of Strings

---
+++ JavaBasicsExamples/src/org/thenewboston/intermediate/CommonStringMethods.java
@@ -0,0 +1,28 @@
+package org.thenewboston.intermediate;
+
+public class CommonStringMethods {
+
+	public static void main(String[] args) {
+		
+		String[] words = {"funk" , "chunk" , "furry" , "baconator"};
+		
+		// Starts With
+		for(String w:words){
+			if(w.startsWith("fu")){
+				System.out.println(w + " starts with fu");
+			}
+		}
+		
+		
+		// Ends With
+		for(String w : words){
+			if(w.endsWith("nk")){
+				System.out.println(w + " ends with nk");
+			}
+		}
+		
+		
+
+	}
+
+}
