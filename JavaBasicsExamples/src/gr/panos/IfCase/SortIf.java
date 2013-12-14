f0056761ef5cc0dc5b2c5d4205ffa4ea3fc7b647
panos <panos@panos-PC>
2013-12-14 19:54:13 +0200

    Sort if and loop

---
+++ JavaBasicsExamples/src/gr/panos/IfCase/SortIf.java
@@ -0,0 +1,15 @@
+package gr.panos.IfCase;
+
+public class SortIf {
+
+	static int a = 105;
+
+	public static void main(String[] args){
+		
+		boolean b = (a>2 && a<20) ? true : false;
+		System.out.println(b);
+
+		
+	}
+		
+}
---
+++ JavaBasicsExamples/src/gr/panos/loops/SortFor.java
@@ -0,0 +1,16 @@
+package gr.panos.loops;
+
+public class SortFor {
+	
+	public static void main(String[] args){
+		
+		String[] words = {"funk" , "chunk" , "furry" , "baconator"};
+		
+		// To words einai ena array kai se kathe iteration i epomeni eggrafi paei stin metabliti w
+		for(String w:words){
+			System.out.println(w);
+		}
+		
+	}
+	
+}
